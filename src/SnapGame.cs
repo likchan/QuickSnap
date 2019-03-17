57744800b8ed06263bcf49e2c9f24fe466838e42
obadiah0313 <101219548@students.swinburne.edu.my>
2019-03-17 21:49:34 +0800

    add draw it in the game

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -24,7 +24,7 @@ namespace CardGames
 
 			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
 			{
-				myGame.FlipNextCard ();
+				myGame.Start ();
 			}
 		}
 
@@ -35,6 +35,7 @@ namespace CardGames
 		private static void DrawGame(Snap myGame)
 		{
 			SwinGame.ClearScreen(Color.White);
+			SwinGame.DrawBitmap ("cardsBoard.png", 0, 0);
 
 			// Draw the top card
 			Card top = myGame.TopCard;

ca371de769e1d041e124fd4b6a9e0800e74ae245
likchan <101219535@students.swinburne.edu.my>
2019-03-19 11:57:46 +0800

    redo the board card location

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -6,6 +6,9 @@ using CardGames.GameLogic;
 /// </summary>
 namespace CardGames
 {
+	/// <summary>
+	/// Snap game.
+	/// </summary>
     public class SnapGame
     {
         public static void LoadResources()
@@ -62,7 +65,7 @@ namespace CardGames
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
-				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 565, 225);
+				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 565, 226);
 			}
 			else
 			{

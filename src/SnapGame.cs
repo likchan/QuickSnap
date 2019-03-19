ce123b4e26fdacc8e7e9b9cde42aa12371bba27a
alberttiong <101219522@students.swinburne.edu.my>
2019-03-19 13:35:17 +0800

    insert chunkfive.otf and set white color for score

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -22,6 +22,7 @@ namespace CardGames
 			SwinGame.LoadSoundEffectNamed ("LeftSlap", "leftslap.wav");
 			SwinGame.LoadSoundEffectNamed ("RightSlap", "rightslap.wav");
 			SwinGame.LoadSoundEffectNamed ("Bothslap", "bothsound.wav");
+			SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24); // font of score//
 		}
 
 		/// <summary>
@@ -70,10 +71,13 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
+				
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 565, 226);
+				SwinGame.DrawText ("" + myGame.Score(0), Color.White, "GameFont",0, 30); // let the score color become white//
+
 			}
 			else
 			{
